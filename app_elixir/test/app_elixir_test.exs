1ca95514aa651c7713c68de56d786ce6193bef5a
gilcierweb <gilcier06@yahoo.com.br>
2018-06-12 14:43:10 -0300

    adding test routes app elixir

--- app_elixir/test/app_elixir_test.exs
+++ app_elixir/test/app_elixir_test.exs
@@ -2,12 +2,36 @@ defmodule AppElixirTest do
   use ExUnit.Case
   doctest AppElixir
 
-  test "greets the world" do
-    assert AppElixir.hello() == :world
-  end
+  import Plug.Test
 
   test "greets the world" do
     assert AppElixir.hello() == :world
   end
+
+  test "/ success" do
+    conn = conn("GET", "/")
+    conn = AppElixir.Router.call(conn, [])
+    assert conn.status == 200
+  end
+
+  test "/gilcierweb success" do
+    conn = conn("GET", "/gilcierweb")
+    conn = AppElixir.Router.call(conn, [])
+    assert conn.status == 200
+  end
+
+  test "/search success" do
+    conn = conn("GET", "/search")
+    conn = AppElixir.Router.call(conn, [])
+    assert conn.status == 200
+  end
+
+#  test "/search returns valid JSON" do
+#    conn = conn("GET", "/search")
+#    conn = AppElixir.Router.call(conn, [])
+#    data = Poison.Parser.parse!(conn.resp_body)
+#    IO.puts data
+#    assert data
+#  end
 
 end
